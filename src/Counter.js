6d022e8e7e043c921cf008bba21bc491cf1e7c2c
Joshkaki00 <josh.kakinuki@students.dominican.edu>
2025-03-27 14:57:30 -0700

    added button to remove counter

--- src/Counter.js
+++ src/Counter.js
@@ -5,8 +5,10 @@ function Counter(props) {
         <h1>{props.value}</h1>
         <button onClick={props.increment}>+</button>
         <button onClick={props.decrement}>-</button>
+        {/* Button to remove this counter */}
+        <button onClick={props.removeCounter}>Remove Counter</button>
       </div>
     );
   }
   
-  export default Counter;
+export default Counter;
