1ce8fe82d7d10ebb33969379c6c0c663fb9b8fc3
Joshkaki00 <josh.kakinuki@students.dominican.edu>
2025-03-27 14:53:11 -0700

    updated counter js

--- src/Counter.js
+++ src/Counter.js
@@ -1,17 +1,12 @@
-import { useState } from 'react';
-
 function Counter(props) {
-  // Initialize count with 0 (or you could use props.value if desired)
-  const [count, setCount] = useState(0);
-
-  return (
-    <div className="Counter">
-      <small>{props.label}</small>
-      <h1>{count}</h1>
-      <button onClick={() => setCount(count + 1)}>+</button>
-      <button onClick={() => setCount(count - 1)}>-</button>
-    </div>
-  );
-}
-
-export default Counter;
+    return (
+      <div className="Counter">
+        <small>{props.label}</small>
+        <h1>{props.value}</h1>
+        <button onClick={props.increment}>+</button>
+        <button onClick={props.decrement}>-</button>
+      </div>
+    );
+  }
+  
+  export default Counter;
