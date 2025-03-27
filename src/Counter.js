2b6eac99f88a272ab7ec88368b6ebe990c4f513e
Joshkaki00 <josh.kakinuki@students.dominican.edu>
2025-03-27 14:50:15 -0700

    counter uses usestate

--- src/Counter.js
+++ src/Counter.js
@@ -1,12 +1,17 @@
+import { useState } from 'react';
+
 function Counter(props) {
-    return (
-      <div className="Counter">
-        <small>{props.label}</small>
-        <h1>{props.value}</h1>
-        <button>+</button>
-        <button>-</button>
-      </div>
-    );
-  }
-  
+  // Initialize count with 0 (or you could use props.value if desired)
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="Counter">
+      <small>{props.label}</small>
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count + 1)}>+</button>
+      <button onClick={() => setCount(count - 1)}>-</button>
+    </div>
+  );
+}
+
 export default Counter;
